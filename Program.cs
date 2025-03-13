2ceb0d010734edb11f58c97af60042a12e918ad4
adarlpz <adarlopz.dev@gmai.com>
2025-03-13 15:09:43 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿char[,] asientos = new char[10, 10];
+
+for (char letra = 'A'; letra < 'A' + 10; letra++) {
+    Console.Write("  " + letra);
+}
+Console.WriteLine();
+
+for (int f = 0; f < 10; f++) {
+    Console.Write(f);
+    for (int c = 0; c < 10; c++) {
+        asientos[f, c] = 'L';
+        Console.Write(" " + asientos[f, c] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.Write("Que asiento quieres reservar?");
+
+string asiento = Console.ReadLine();
+
+char fila = asiento[0];
+char columna = asiento[1];
+
