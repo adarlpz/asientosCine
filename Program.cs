727c002d3fa516c99f5ca2c243adca24cdeafd88
adarlpz <adarlopz.dev@gmai.com>
2025-03-21 08:58:55 -0600

    ya aparta y cambia de estado un asiento, y tmb ya esta el while: falta seguridad

--- Program.cs
+++ Program.cs
@@ -1,23 +1,46 @@
-﻿char[,] asientos = new char[10, 10];
+﻿internal class Program {
+    private static void Main(string[] args) {
+        char[,] asientos = new char[10, 10];
+        for (int f = 0; f < 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                asientos[f, c] = 'L';
+            }
+        }
 
-for (char letra = 'A'; letra < 'A' + 10; letra++) {
-    Console.Write("  " + letra);
-}
-Console.WriteLine();
+        void mostrarAsientos() {
+            for (char letra = 'A'; letra < 'A' + 10; letra++) {
+                Console.Write("  " + letra);
+            }
+            Console.WriteLine();
+            for (int f = 0; f < 10; f++) {
+                Console.Write(f);
+                for (int c = 0; c < 10; c++) {
+                    Console.Write(" " + asientos[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        mostrarAsientos();
 
-for (int f = 0; f < 10; f++) {
-    Console.Write(f);
-    for (int c = 0; c < 10; c++) {
-        asientos[f, c] = 'L';
-        Console.Write(" " + asientos[f, c] + " ");
+        while (true) {
+            Console.WriteLine();
+            Console.Write("Que asiento quieres reservar? ");
+            string asiento = Console.ReadLine();
+            char fila = asiento[0];
+            char columna = asiento[1];
+
+            if (new[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" }.Contains(fila.ToString().ToUpper()))
+            { //intercambio
+                char temp = fila;
+                fila = columna;
+                columna = temp;
+            }
+            int nuevaFila = fila - '0';
+            int nuevaColumna = columna - 'A';
+
+            asientos[nuevaFila, nuevaColumna] = 'X';
+
+            mostrarAsientos();
+        }
     }
-    Console.WriteLine();
 }
-Console.WriteLine();
-Console.Write("Que asiento quieres reservar?");
-
-string asiento = Console.ReadLine();
-
-char fila = asiento[0];
-char columna = asiento[1];
-
